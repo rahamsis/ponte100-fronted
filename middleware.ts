e4c7ed2bf0aa84d703322aa95290914d300bcdf5
Rahamsis PC <rahamsiscg.95@gmail.com>
2025-07-08 23:11:10 -0500

    adding maintenance

--- middleware.ts
+++ middleware.ts
@@ -1,75 +1,61 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { getToken } from "next-auth/jwt";
 
-export async function middleware(req: NextRequest) {
-    const pathname = req.nextUrl.pathname;
+// export async function middleware(req: NextRequest) {
+//     // Obtener token con req
+//     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+//     if (!token) return NextResponse.redirect(new URL("/", req.url));
 
-    // Evita ejecutar middleware en la página de mantenimiento o recursos estáticos
-    if (
-        pathname.startsWith("/maintenance") ||
-        pathname.startsWith("/_next") ||
-        pathname.startsWith("/api") || // <-- AÑADIDO: evitar bucle con fetch interno
-        pathname === "/favicon.ico" ||
-        pathname === "/mantenimiento.png"
-    ) {
-        return NextResponse.next();
-    }
+//     try {
+//         // Lógica existente para validar que la sesión está activ
+//         const res = await fetch(`${process.env.NEXTAUTH_URL}/api/session?sessionToken=${token.sessionToken}`);
+//         const data = await res.json();
 
+//         if (!data.active) {
+//             return NextResponse.redirect(new URL("/", req.url));
+//         }
 
-    // Obtener token con req
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-    if (!token) return NextResponse.redirect(new URL("/", req.url));
+//         const pathname = req.nextUrl.pathname;
+//         const welcomeCookie = req.cookies.get("welcome");
 
-    try {
-        // Lógica existente para validar que la sesión está activ
-        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/session?sessionToken=${token.sessionToken}`);
-        const data = await res.json();
+//         // ✅ Permitir el acceso a /bienvenida si welcome es 0
+//         if (welcomeCookie?.value === "0" && (!pathname.startsWith("/bienvenida"))) {
+//             return NextResponse.redirect(new URL("/bienvenida", req.url));
+//         }
 
-        if (!data.active) {
-            return NextResponse.redirect(new URL("/", req.url));
-        }
+//         if(pathname.startsWith("/bienvenida") && welcomeCookie?.value !== "0"){
+//              return NextResponse.redirect(new URL("/inicio", req.url));
+//         }
 
-        const pathname = req.nextUrl.pathname;
-        const welcomeCookie = req.cookies.get("welcome");
+//     } catch (error) {
+//         console.error("Error en el middleware:", error);
+//         return NextResponse.redirect(new URL("/error", req.url));
+//     }
 
-        // ✅ Permitir el acceso a /bienvenida si welcome es 0
-        if (welcomeCookie?.value === "0" && (!pathname.startsWith("/bienvenida"))) {
-            return NextResponse.redirect(new URL("/bienvenida", req.url));
-        }
+//     return NextResponse.next();
+// }
 
-        if (pathname.startsWith("/bienvenida") && welcomeCookie?.value !== "0") {
-            return NextResponse.redirect(new URL("/inicio", req.url));
-        }
-
-    } catch (error) {
-        console.error("Error en el middleware:", error);
-        return NextResponse.redirect(new URL("/error", req.url));
-    }
-
-    return NextResponse.next();
-}
-
-export const config = {
-    // runtime: "nodejs",
-    matcher: [
-        '/actividades',
-        '/bienvenida', '/bienvenida/:idGrado*',
-        '/control-de-habilidades', '/control-de-habilidades/:idTema*',
-        '/despierta-tu-inteligencia', '/despierta-tu-inteligencia/:idTema*',
-        '/estadisticas',
-        '/examenes-no-repetidos', '/examenes-no-repetidos/:idExamen*',
-        '/inicio',
-        '/practica-un-tema',
-        '/preguntas-fallidas',
-        '/primer-simulacro',
-        '/primera-practica',
-        '/progreso',
-        '/registro',
-        '/result',
-        '/talleres-de-estudio', '/talleres-de-estudio/:idTema*',
-        '/temario',
-        '/videos',
-        '/configuracion',
-    ],
-};
+// export const config = {
+//     // runtime: "nodejs",
+//     matcher: [
+//         '/actividades',
+//         '/bienvenida', '/bienvenida/:idGrado*',
+//         '/control-de-habilidades','/control-de-habilidades/:idTema*',
+//         '/despierta-tu-inteligencia','/despierta-tu-inteligencia/:idTema*',
+//         '/estadisticas',
+//         '/examenes-no-repetidos','/examenes-no-repetidos/:idExamen*',
+//         '/inicio', 
+//         '/practica-un-tema', 
+//         '/preguntas-fallidas', 
+//         '/primer-simulacro', 
+//         '/primera-practica',
+//         '/progreso', 
+//         '/registro', 
+//         '/result', 
+//         '/talleres-de-estudio', '/talleres-de-estudio/:idTema*',
+//         '/temario',
+//         '/videos',
+//         '/configuracion',
+//     ],
+// };
