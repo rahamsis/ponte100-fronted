67250775de2b92244b9cb7205bd91f393b5ade34
Rahamsis PC <rahamsiscg.95@gmail.com>
2025-07-08 22:41:20 -0500

    adding maintenance

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -2,7 +2,7 @@ export default function MaintenancePage() {
   return (
     <main style={{ textAlign: 'center', padding: '10%' }}>
       <h1 style={{fontWeight:'bold', color:'#10439c'}}>🚧 En mantenimiento</h1>
-      <p style={{color:'#10439c', fontSize:'20px'}} >Estamos actualizando la plataforma. Por favor, vuelve más tarde.</p>
+      <p style={{color:'#10439c', fontSize:'20px'}}>Estamos actualizando la plataforma. Por favor, vuelve más tarde.</p>
       <img
         src="/images/mantenimiento.png"
         alt="Página en mantenimiento"
